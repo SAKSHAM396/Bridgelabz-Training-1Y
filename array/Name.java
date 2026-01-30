009d80d707e17523e863e38cf4c299b0255eea70
unknown <msaksham656@gmail.com>
2026-01-30 10:00:42 +0530

    Names

---
+++ array/Name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Name{
+	public static void main(String args[]){
+	String [] names ={"Arjun","Saksham","Harshit","Haris","Lukesh"};
+	for(int i=0;i<=names.length;i++){
+		System.out.println(names[i]);
+	}
+	}
+}
