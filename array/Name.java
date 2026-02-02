5c44d2b193bd272f51b73b788c0dd7e996e0fda6
Saksham Mangla <msaksham656@gmail.com>
2026-02-02 21:25:07 +0530

    Added out folder

--- array/Name.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Name{
-	public static void main(String args[]){
-	String [] names ={"Arjun","Saksham","Harshit","Haris","Lukesh"};
-	for(int i=0;i<=names.length;i++){
-		System.out.println(names[i]);
-	}
-	}
-}
